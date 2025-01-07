0157c8ad36dd2cf8d08fd095924ecbc330285467
xanmoy <xanmoy.tsx@gmail.com>
2025-01-08 01:45:57 +0530

    updated

--- app/notes/[slug]/page.tsx
+++ app/notes/[slug]/page.tsx
@@ -12,13 +12,17 @@ export default async function BlogPost({ params }: { params: { slug: string } })
 
     return (
         <article className="prose lg:prose-xl mb-72 mx-auto mt-10 p-5 rounded-md justify-center items-center text-center">
-            <h1 className="text-4xl font-bold text-center text-gray-800 dark:text-gray-200 mb-4">{post.title}</h1>
-            <p className="text-sm text-center text-gray-500 dark:text-gray-400 mb-6">
+            <h1 className="text-4xl font-bold text-center text-gray-800 dark:text-gray-200 mb-4"
+                style={{ fontFamily: "PT Sans, serif" }}
+            >{post.title}</h1>
+            <p className="text-sm text-center text-gray-500 dark:text-gray-400 mb-6"
+                style={{ fontFamily: "PT Sans, serif" }}>
                 Published on {new Date(post.date).toLocaleDateString()}
             </p>
             <div
-                className="text-gray-700 dark:text-gray-300 mx-auto w-full sm:w-3/4 lg:w-2/3"
+                className="text-gray-700 text-3xl dark:text-gray-300 mx-auto w-full sm:w-3/4 lg:w-2/3"
                 dangerouslySetInnerHTML={{ __html: post.content }}
+                style={{ fontFamily: "PT Sans, serif" }}
             />
         </article>
     );
